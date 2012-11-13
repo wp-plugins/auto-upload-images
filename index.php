a44fe64d754680a740addfdcbe2a9b60abf82667
airani <airani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-13 21:42:13 +0000

    Add image to Media Library and Attach to post Fix a bug Add tags/1.1
    git-svn-id: https://plugins.svn.wordpress.org/auto-upload-images/tags/1.1@624872 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: Auto Upload Images
+Plugin URI: http://wordpress.org/extend/plugins/auto-upload-images/
+Description: Automatically upload external images of a post to wordpress upload directory
+Version: 1.1
+Author: Ali Irani
+Author URI: http://p30design.net
+License: GPLv2 or later
+*/
+
+add_action( 'save_post', 'wp_auto_upload_images' );
+
+/**
+ * Automatically upload external images of a post to wordpress upload directory
+ *
+ * @param $post_id
+ */
+function wp_auto_upload_images($post_id) {
+
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+		return;
+		
+	global $wpdb;
+	
+	$content = $wpdb->get_var( "SELECT post_content FROM wp_posts WHERE ID='$post_id' LIMIT 1" );
+	$images_url = wp_get_images_url($post_id, $content);
+	
+	if($images_url) {
+		foreach ($images_url as $image_url) {
+			if(!wp_is_myurl($image_url) && $new_image_url = wp_save_image($image_url, $post_id)) {
+				$new_images_url[] = $new_image_url;
+				unset($new_image_url);
+			} else {
+				$new_images_url[] = $image_url;
+			}
+		}
+	
+		$total = count($new_images_url);
+		
+		for($i=0; $i<=$total-1; $i++) {
+			$content = preg_replace('/'. preg_quote($images_url[$i], '/') .'/', $new_images_url[$i], $content);
+		}
+		
+		remove_action( 'save_post', 'wp_auto_upload_images' );
+-		wp_update_post( array('ID' => $post_id, 'post_content' => $content) );
+-		add_action( 'save_post', 'wp_auto_upload_images' );
+	}
+}
+
+/**
+ * Detect url of images which exists in content
+ *
+ * @param $post_id
+ * @return array of urls or false
+ */
+function wp_get_images_url( $post_id, $content ) {
+	preg_match_all('/<img[^>]*src=("|\')([^(\?|#|"|\')]*)(\?|#)?[^("|\')]*("|\')[^>]*\/>/', $content, $urls, PREG_SET_ORDER);
+	
+	if(is_array($urls)) {
+		foreach ($urls as $url)
+			$images_url[] = $url[2];
+	}
+	
+	return isset($images_url) ? $images_url : false;
+}
+
+/**
+ * Check url is internal or external
+ *
+ * @param $url
+ * @return true or false
+ */
+function wp_is_myurl( $url ) {
+	$url_pattern = "/https?:\/\/((.*?)\.)*([a-z0-9\-]+\.[a-z]+)\/?.*/i";
+	$myurl = get_bloginfo('url');
+	
+	if(preg_match($url_pattern, $myurl, $m))
+		$myurl = $m[3];
+	else
+		return false;
+	
+	if(preg_match($url_pattern, $url, $m))
+		$target_url = $m[3];
+	else
+		return false;
+	
+	return ($myurl == $target_url) ? true : false;
+}
+
+/**
+ * Save image on wp_upload_dir
+ * Add image to Media Library and attach to post
+ *
+ * @param $url
+ * @param $post_id
+ * @return new $url or false
+ */
+function wp_save_image($url, $post_id = 0) {
+	if(preg_match('/\/([a-z0-9\-_\+\.]+\.[a-z0-9]+)$/i', $url, $m))
+		$image_name = $m[1];
+	else 
+		return false;
+	
+	$upload_dir = wp_upload_dir(date('Y/m'));
+	$path = $upload_dir['path'] . '/' . $image_name;
+	$new_image_url = $upload_dir['url'] . '/' . $image_name;
+	
+	if(file_exists($path))
+		return false;
+	
+	if(function_exists('curl_init')) {
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		file_put_contents($path, $data);
+		
+		$wp_filetype = wp_check_filetype($new_image_url);
+		$filename = $upload_dir['path'];
+		$attachment = array(
+			'guid' => $new_image_url, 
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title' => preg_replace('/\.[^.]+$/', '', basename($new_image_url)),
+			'post_content' => '',
+			'post_status' => 'inherit'
+		);
+		wp_insert_attachment($attachment, $new_image_url, $post_id);
+		
+		return $new_image_url;
+	} else {
+		return false;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Auto Upload Images ===
+Contributors: airani
+Donate link: http://p30design.net/about/contact
+Tags: upload, auto, image, automaticlly, images, admin, post
+Requires at least: 2.7
+Tested up to: 3.4.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+When you want to save post, this plugin search for images url which exists in post and automatically upload external images to wordpress upload directory and replace external link with your link.
+
+Automatically add image to Media Library and Attach to post.
+
+== Installation ==
+
+Upload the "Auto Upload Images" plugin to your blog, Activate it.
+
+1, 2, 3: You're done!
+
+== Frequently Asked Questions ==
+
+= Are there special settings for the plugin? =
+
+No, Just Activate it :)
+
+== Changelog ==
+
+= 1.1 =
+
+* Add image to Media Library and attach to post
+* Fix a bug
+
+= 1.0 =
+
+* It's first version.
